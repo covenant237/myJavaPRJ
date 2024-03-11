3ce6bd4d2b41fc5b6b3066a6a5839af1241a255b
covenant237 <church6754@gmail.com>
2024-03-11 15:06:43 +0900

    my second code

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,7 +1,9 @@
 public class Hello {
 
     public static void main(String[] args){
-        System.out.println("I Wanna Fight!!");
+        System.out.println("I like martial arts!!");
+
+        System.out.println("this is my life!!");
 
         }
 }
